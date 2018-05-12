ba517406d62e6d21d1e5f8510afc1e88dee8861e
Paul Smith <paulcsmith0218@gmail.com>
2018-05-12 08:54:35 -0400

    Upgrade to v0.10.0-rc1

--- src/actions/home/index.cr
+++ src/actions/home/index.cr
@@ -1,5 +1,12 @@
 class Home::Index < BrowserAction
+  include Auth::SkipRequireSignIn
+  unexpose current_user
+
   get "/" do
-    render Lucky::WelcomePage
+    if current_user?
+      redirect Me::Show
+    else
+      render Lucky::WelcomePage
+    end
   end
 end
